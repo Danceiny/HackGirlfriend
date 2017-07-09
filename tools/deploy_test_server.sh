9e41f3ee61f0a4eb775668d1b7e48b7bb9e3b3be
Danceiny <danceiny@gmail.com>
2017-07-09 20:52:05 +0800

    重启uwsgi的正确姿势(supervisorctl reload不可行)

--- tools/deploy_test_server.sh
+++ tools/deploy_test_server.sh
@@ -7,10 +7,12 @@ ssh -i /mnt/c/Developer/workspaces/密钥/for_danceiny ubuntu@118.89.27.96 '
     echo "-----------更新代码-----------"
     git pull
     echo "-----------重启uwsgi-----------"
-    sudo supervisorctl reload
+    sudo killall  -9 uwsgi
+    sudo service supervisor restart
     echo "-----------重启uwsgi成功，结束-----------"
     exit
 '
 # netstat -ap | grep 9001 | grep CLOSE_WAIT|grep -v grep|cut -c 80-84 |xargs kill
 #sudo killall  -9 uwsgi
 #sudo service supervisor restart
+#sudo supervisorctl reload
